dd7cc9380e40a35730c8e6e1d31e0e2b9275eb44
Andrew <andrey.schule@gmail.com>
2022-11-16 10:44:18 +0100

    Adding movement to the player (basic)

--- Assets/Scripts/ClickAndMove.cs
+++ Assets/Scripts/ClickAndMove.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class ClickAndMove : MonoBehaviour
 {
+    public Vector3 targetPosition = new Vector3 (0f, 0f, 0f);
+    public Transform player;
+    public int speed = 1;
+
+//Update und Start
+    public void Start()
+    {
+
+    }
+
     public void Update()
     {
-        OnMouseHover();
+        OnMouseClick();
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 10);
+        }
+        player.position = Vector3.MoveTowards(player.position, targetPosition, Time.deltaTime * speed);
     }
-    void OnMouseHover()
+
+    public void FixedUpdate()
+    {
+        
+    }
+
+//Events
+
+    void OnMouseClick()
     {
         if (Input.GetMouseButtonDown(0)) print("Mouse Clicked!");
     }
